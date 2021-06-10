46341d7cae66e0d9382b98b840fcf590a94cfe17
Avner José Guimarães Ribeiro <avnerjose@pop-os.localdomain>
2021-06-09 23:01:12 -0300

    Teste 4 real

--- src/agenciabancaria/AgenciaBancaria.java
+++ src/agenciabancaria/AgenciaBancaria.java
@@ -6,5 +6,6 @@ public class AgenciaBancaria {
         System.out.println("Teste");
         System.out.println("Teste 2");
         System.out.println("Teste 3");
+        System.out.println("Teste 4");
     }
 }
