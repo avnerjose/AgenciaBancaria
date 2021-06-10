4fcf0feaca3819cad43f08e49f562a272a7298fa
Fabio <Fabio@Fabio>
2021-06-09 22:51:26 -0300

    sout teste 3

--- src/agenciabancaria/AgenciaBancaria.java
+++ src/agenciabancaria/AgenciaBancaria.java
@@ -5,5 +5,6 @@ public class AgenciaBancaria {
     public static void main(String[] args) {
         System.out.println("Teste");
         System.out.println("Teste 2");
+        System.out.println("Teste 3");
     }
 }
