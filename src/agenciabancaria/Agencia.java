c4cf18fa3dae16cbf2c431eeb65e1ead9a2bf9a6
Avner José Guimarães Ribeiro <avnerjose@pop-os.localdomain>
2021-06-15 15:53:32 -0300

    Preenchencdo classe Agencia

--- src/agenciabancaria/Agencia.java
+++ src/agenciabancaria/Agencia.java
@@ -1,5 +1,10 @@
 package agenciabancaria;
 
+import java.util.ArrayList;
+
 public class Agencia {
 
+    private String nome;
+    private String cidade;
+    private ArrayList<Conta> contas;//Conjunto de contas que a agência oferece
 }
