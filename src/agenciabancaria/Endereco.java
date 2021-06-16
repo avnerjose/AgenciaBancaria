9e120944c800395487ac7f07a803f7988e8381d2
Fabio <Fabio@Fabio>
2021-06-16 19:56:15 -0300

    Criação das classes Cliente, Empréstimo e Endereço

--- src/agenciabancaria/Endereco.java
+++ src/agenciabancaria/Endereco.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package agenciabancaria;
 
-/**
- *
- * @author Fabio
- */
 public class Endereco {
-    
+    private String cidade;
+    private String bairro;
+    private String rua;
+    private int numero;
+    private String cep;
 }
